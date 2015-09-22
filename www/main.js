6e4cc19b79b6fc589cf94afd0e39b3d7f55577c0
daiiz <da.iz.mg@gmail.com>
2015-09-23 01:08:43 +0900

    見えていないところは非表示にしておく作戦

--- www/main.js
+++ www/main.js
@@ -19,26 +19,30 @@ $(function () {
     var WIDTH = Math.min(window.innerHeight * 0.6, 600);
     var $contentsPage = $('#contents-page');
     // .photo-frame
-    var temp_photoFrame = '<div class="content" style="width:{}px">{}{}<div style="width: {}px; height: {}px" class="photo-frame">{}</div>{}</div>';
+    var temp_photoFrame = '<div class="content" id="content-{}" style="width:{}px">{}{}<div style="width: {}px; height: {}px" class="photo-frame">{}</div>{}</div>';
     var temp_day        = '<div class="day">{}</div>';
     var temp_title      = '<div class="title">{}</div>';
-    var temp_photo      = '<img src="{}" class="photo">';
+    var temp_photo      = '<a href="{}" target="_blank"><img src="{}" class="photo"></a>';
     var temp_diary      = '<div class="diary">{}</div>';
 
     var miilDiary = {
         contents: user_contents,
         biggestFav: 0,
+        photoNums: 0,
 
         addPhotoFrame: function (content) {
             var day = content.posted;
             day = temp_day.format(day[0] +'-'+ day[1] +'-'+ day[2]);
             var title = temp_title.format(content.title);
             var photo = 'photos/'+ content.date + '.jpg';
-            photo = temp_photo.format(photo);
+            photo = temp_photo.format(content.page, photo);
             var diary = temp_diary.format(content.diary);
 
-            var photoFrame = temp_photoFrame.format(WIDTH, day, title, WIDTH, WIDTH, photo, diary);
+            var photoFrame = temp_photoFrame.format(this.photoNums, WIDTH, day, title, WIDTH, WIDTH, photo, diary);
             $contentsPage.append(photoFrame);
+
+            this.bindEvents('content-' + this.photoNums);
+            this.photoNums++;
         },
 
         setBiggestFavNum: function () {
@@ -51,7 +55,39 @@ $(function () {
         },
 
         init: function (settings) {
-            
+            $('.toptitle').text(settings.title);
+            this.bindEvent_toppage();
+        },
+
+        // 表示エリア外の画像以外は非表示にしておく
+        bindEvents: function (elemId) {
+            $('#' + elemId).on('inview', function (event, isInView, visiblePartX, visiblePartY) {
+                if (isInView) {
+                    if (visiblePartX == 'left' && visiblePartY == 'both'){
+                        $('#' + elemId).find('.photo').fadeIn('slow');
+                    }
+                    if ((visiblePartX == 'right' || visiblePartX == 'both') && visiblePartY == 'both'){
+                        $('#' + elemId).find('.photo').fadeIn();
+                    }
+                } else {
+                    $('#' + elemId).find('.photo').css({display: 'none'});//fadeOut();
+                }
+            });
+        },
+
+        bindEvent_toppage: function () {
+            /*$('.toppage').on('inview', function (event, isInView, visiblePartX, visiblePartY) {
+                if (isInView) {
+                    if (visiblePartX == 'left' && visiblePartY == 'both'){
+                        $('.toppage').fadeIn('slow');
+                    }
+                    if ((visiblePartX == 'right' || visiblePartX == 'both') && visiblePartY == 'both'){
+                        $('.toppage').fadeIn();
+                    }
+                } else {
+                    $('.toppage').css({display: 'none'});
+                }
+            });*/
         }
     };
 
@@ -61,6 +97,8 @@ $(function () {
     var settings = miilDiary.contents[0];
     miilDiary.contents.shift();
 
+    // 初期化
+    miilDiary.init(settings);
     miilDiary.contents.reverse();
 
     miilDiary.contents.forEach(function (content) {
